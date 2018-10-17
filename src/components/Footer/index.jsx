26375f007b4dc84625b257c45babb49f3ea34fb5
gustavovieira <gustavovieira992@gmail.com>
2018-10-17 09:18:02 -0300

    first commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row gap-y align-items-center">
+
+                    <div class="col-12 col-lg-6 offset-lg-3">
+                        <ul class="nav nav-primary nav-hero">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.html">Bahdcasts</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Welcome from './components/Welcome';
+import CreateArticle from './components/CreateArticle';
+import Login from './components/Login';
+import SingleArticle from './components/SingleArticle';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Navbar />
+            <Route exact path="/" component={Welcome} />
+            <Route path="/login" component={Login} />
+            <Route path="/article/:id" component={SingleArticle} />
+            <Route path="/articles/create" component={CreateArticle} />
+            <Footer />
+        </div>
+    </BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
